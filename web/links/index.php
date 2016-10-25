70b55763d1f75e951b992cb4dfdba6789e8db4ce
James Nash <cirrus@twiddles.com>
2016-10-25 20:29:31 +0100

    Removed dead link to Yvette's old website.

--- web/links/index.php
+++ web/links/index.php
@@ -7,7 +7,6 @@
 	<h2>Link love</h2>
 	<p>This page will be updated very soon! In the mean time, here's a random selection of links:</p>
 	<ul>
-		<li><a href="http://yvette.twiddles.com/">Yvette Park</a>, my lovely girlfriend.</li>
 		<li><a href="http://www.twiddles.com/">Twiddles</a> - <q>Coming soon</q>, honest guv'!</li>
 		<li><a href="http://twiddles.spreadshirt.net/">The official Twiddles shop</a>!</li>
 		<li><a href="http://www.hyper-timer.com/">Hyper Timer</a> - the awesome countdown timer app by Twiddles.</li>
