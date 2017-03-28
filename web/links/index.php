ff5464330614d17b1d0c3fa1a2543d2771c3cc5b
James Nash <cirrus@twiddles.com>
2017-03-28 22:34:55 +0100

    Fixed up yet more links

--- web/links/index.php
+++ web/links/index.php
@@ -8,7 +8,7 @@
 	<p>This page will be updated very soon! In the mean time, here's a random selection of links:</p>
 	<ul>
 		<li><a href="http://www.twiddles.com/">Twiddles</a> - <q>Coming soon</q>, honest guv'!</li>
-		<li><a href="https://shop.spreadshirt.net/twiddles/">The official Twiddles shop</a>!</li>
+		<li><a href="https://shop.spreadshirt.co.uk/twiddles/">The official Twiddles shop</a>!</li>
 		<li><a href="http://www.hyper-timer.com/">Hyper Timer</a> - the awesome countdown timer app by Twiddles.</li>
 		<li><a href="http://adonthell.nongnu.org/">Adonthell</a>. A cool, open-source RPG adventure game.</li>
 	</ul>	
