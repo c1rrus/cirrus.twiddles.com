8a4b9398555d4e8742ea78e68f2135160168aaea
James Nash <cirrus@twiddles.com>
2020-06-03 12:33:59 +0100

    feat: makes site.url adapt to build env

--- src/site/_data/site.js
+++ src/site/_data/site.js
@@ -1,3 +1,9 @@
+// Figure out our base URL from (in order of
+// priority):
+// - Netlify env var (DEPLOY_PRIME_URL)
+// - local dev URL
+const siteUrl = process.env['DEPLOY_PRIME_URL'] || 'http://localhost:8080'
+
 module.exports = {
-  url: 'http://cirrus.twiddles.com'
-}
+  url: siteUrl
+};
