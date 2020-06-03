23960c206315a9539c137c205aed565161e83905
James Nash <cirrus@twiddles.com>
2020-06-03 18:31:33 +0100

    fix: siteUrl on prod builds

--- src/site/_data/site.js
+++ src/site/_data/site.js
@@ -1,16 +1,18 @@
+// If Netlify's CONTEXT env var says this is production,
+// we're deploying to the public site. All other cases
+// (preview, local dev, etc.) are considered non-production
+const isProduction = process.env['CONTEXT'] === 'production';
+
 // Figure out our base URL from (in order of
 // priority):
-// - Netlify env var (DEPLOY_PRIME_URL)
+// - Netlify env var (URL for prod, or else DEPLOY_PRIME_URL)
 // - local dev URL
-const siteUrl = process.env['DEPLOY_PRIME_URL'] || 'http://localhost:8080'
+const siteUrl = (isProduction ? process.env['URL'] : process.env['DEPLOY_PRIME_URL']) || 'http://localhost:8080'
 
 
 
 module.exports = {
   url: siteUrl,
 
-  // If Netlify's CONTEXT env var says this is production,
-  // we're deploying to the public site. All other cases
-  // (preview, local dev, etc.) are considered non-production
-  isProduction: (process.env['CONTEXT'] === 'production')
+  isProduction,
 };
