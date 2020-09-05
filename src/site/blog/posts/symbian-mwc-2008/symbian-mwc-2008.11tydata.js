a6c7f461c6c47fb87c461cf4731ea54e544d4e37
James Nash <cirrus@twiddles.com>
2020-09-05 22:23:34 +0100

    fix: workaround 11ty computed data weirdness

--- src/site/blog/posts/symbian-mwc-2008/symbian-mwc-2008.11tydata.js
+++ src/site/blog/posts/symbian-mwc-2008/symbian-mwc-2008.11tydata.js
@@ -1,6 +1,12 @@
+// Workaround for 11ty bug (?), where parent dir's computed data is
+// not executed.
+const postsComputedData = require('../posts.11tydata').eleventyComputed;
+
 module.exports = {
 
   eleventyComputed: {
+    ...postsComputedData,
+
     keywords: data => {
       const deduped = new Set(['symbian', 'mwc', 'mobile world congress', 'mobile', 'smartphone']);
       if (data.keywords) {
